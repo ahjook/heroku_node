29e2e63dda8b2462531276a5c57f4a2bc82e91cf
ahjook <merrycris.ajoc@student.passerellesnumeriques.org>
2019-09-24 17:51:40 +0800

    go

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var app = require('express')();
 var http = require('http').createServer();
-var porter = process.env.PORT || 3000; 
+var port = process.env.PORT || 3000; 
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
   });
 
-http.listen(porter, function(){
-  console.log('listening on *: ' + porter);
+http.listen(port, function(){
+  console.log('listening on *: ' + port);
 });

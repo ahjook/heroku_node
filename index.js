f8f2bd53cabfef0218ea66d31aefaadde02e0776
ahjook <merrycris.ajoc@student.passerellesnumeriques.org>
2019-09-24 17:36:10 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var app = require('express')();
 var http = require('http').createServer();
-var ports = process.env.PORT || 3000 
+var port = process.env.PORT || 3000 
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
   });
 
-http.listen(ports, function(){
+http.listen(port, function(){
   console.log('listening on *: ' + port);
 });

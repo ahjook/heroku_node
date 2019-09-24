8aadaaee9e2580d85b7ca1c9ead72c576c341ea7
ahjook <merrycris.ajoc@student.passerellesnumeriques.org>
2019-09-24 17:56:40 +0800

    fgo

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var app = require('express')();
 var http = require('http').createServer();
-var port = process.env.PORT || 3000; 
+var ports = process.env.PORT || 3000; 
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
   });
 
-http.listen(port, function(){
-  console.log('listening on *: ' + port);
+http.listen(ports, function(){
+  console.log('listening on *:' + ports);
 });
